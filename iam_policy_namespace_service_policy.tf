7bd24a33f579b72b33f824489cee8e33ce7ae547
Jacob Johansen <jacob.johansen@virginpulse.com>
2020-09-01 09:27:38 -0500

    fix formatting

--- iam_policy_namespace_service_policy.tf
+++ iam_policy_namespace_service_policy.tf
@@ -111,11 +111,13 @@ data "aws_iam_policy_document" "namespace_service_policy_document" {
 
   statement {
     sid = "s3"
+
     actions = [
       "s3:PutObject",
       "s3:GetObject",
-      "s3:DeleteObject"
+      "s3:DeleteObject",
     ]
+
     resources = [
       "arn:aws:s3:*:${var.account_id}:${local.prefix}-${local.namespace}/*",
     ]
